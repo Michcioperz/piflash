eba3515d4dc790b1a78917166a4380e126735562
Michał Sidor <michcioperz@autistici.org>
2015-05-10 15:12:40 +0200

    landscape yelled

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 if not os.path.exists(os.path.join('static','out.log')):
-	with open(os.path.join('static', 'out.log'), 'w') as f:
-		f.write("")
+    with open(os.path.join('static', 'out.log'), 'w') as f:
+        f.write("")
 if not os.path.exists(os.path.join('static','err.log')):
-	with open(os.path.join('static', 'err.log'), 'w') as f:
-		f.write("")
+    with open(os.path.join('static', 'err.log'), 'w') as f:
+        f.write("")
 
 @app.route('/make', methods=['POST','GET'])
 def make():
-	if request.method == 'POST':
-		with open('static/main.cpp', 'w') as file:
-			file.write(request.form['code'].encode('utf8'))
-		subprocess.call(['bash', 'doit.sh'])
-		return redirect(url_for('index'))
-	return render_template('form.html')
+    if request.method == 'POST':
+        with open('static/main.cpp', 'w') as f:
+            file.write(request.form['code'].encode('utf8'))
+        subprocess.call(['bash', 'doit.sh'])
+        return redirect(url_for('index'))
+    return render_template('form.html')
 
 @app.route("/")
 def index():
@@ -27,13 +27,13 @@ def index():
 
 @app.route("/stdout")
 def stdout():
-	with open('static/out.log') as outlog:
-		return outlog.read()
+    with open('static/out.log') as outlog:
+        return outlog.read()
 
 @app.route("/stderr")
 def stderr():
-	with open('static/err.log') as errlog:
-		return errlog.read()
+    with open('static/err.log') as errlog:
+        return errlog.read()
 
 if __name__ == '__main__':
     parser = ArgumentParser()
